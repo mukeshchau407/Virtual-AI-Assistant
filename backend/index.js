c7ea44168848a5b868264b5b04818bb076968eab
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-08-26 22:27:54 +0545

    Add backend folder

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!")
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
